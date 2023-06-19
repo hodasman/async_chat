95538b5a782fcb244a6c547ed758b093f02bca84
Dmitry <hodas.work@gmail.com>
2023-06-19 19:58:32 +0300

    add task9_1

---
+++ task9_1.py
@@ -0,0 +1,31 @@
+"""
+1. Написать функцию host_ping(), в которой с помощью утилиты ping будет проверяться доступность сетевых узлов.
+ Аргументом функции является список, в котором каждый сетевой узел должен быть представлен именем хоста или ip-адресом.
+  В функции необходимо перебирать ip-адреса и проверять их доступность с выводом соответствующего сообщения
+  («Узел доступен», «Узел недоступен»). При этом ip-адрес сетевого узла должен создаваться с помощью функции
+  ip_address().
+"""
+
+
+import ipaddress
+import subprocess
+
+
+def host_ping(lst_ip_adress: list, timeout: float, count: int):
+    for host in lst_ip_adress:
+        try:
+            adress = ipaddress.ip_address(host)
+        except ValueError:
+            adress = host
+        proc = subprocess.Popen(['ping', f'{adress}', f'-t {timeout}', f'-c {count}'], shell=False,
+                                stdout=subprocess.PIPE)
+        proc.wait()
+        if proc.returncode == 0:
+            print(f'{host} - узел доступен')
+        else:
+            print(f'{host} - узел не доступен')
+
+
+if __name__ == "__main__":
+    lst = ['192.168.100.1', 'yandex.ru', '2.2.2.2', '192.168.0.55']
+    host_ping(lst, 2.0, 1)
