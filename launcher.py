e1a3c2d904915623242ab65b09872505fa014946
Dmitry <hodas.work@gmail.com>
2023-06-24 10:33:28 +0300

    edit launcher

--- launcher.py
+++ launcher.py
@@ -13,6 +13,7 @@ CHOICE_TEXT = """
 CLIENTS = []
 SERVER = ''
 PATH_TO_FILE = os.path.dirname(__file__)
+print(PATH_TO_FILE)
 PATH_TO_SCRIPT_SERVER = os.path.join(PATH_TO_FILE, "server.py")
 PATH_TO_SCRIPT_CLIENTS = os.path.join(PATH_TO_FILE, "client.py")
 
@@ -39,7 +40,7 @@ while True:
             CLIENTS.append(
                 Popen(
                     f'osascript -e \'tell application "Terminal" to do'
-                    f' script "python3 {PATH_TO_SCRIPT_CLIENTS} -m send"\'',
+                    f' script "python3 {PATH_TO_SCRIPT_CLIENTS} -n send"\'',
                     shell=True))
             # Задержка для того, что бы отправляющий процесс успел
             # зарегистрироваться на сервере, и потом в словаре имен
