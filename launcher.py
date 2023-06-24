202d7e559ad7f50bcf2a57a0b8d3b0e9c52a5e75
Dmitry <hodas.work@gmail.com>
2023-06-24 10:18:44 +0300

    launcher for start server and 4 clients

--- launcher.py
+++ launcher.py
@@ -1,23 +1,58 @@
-"""Программа-лаунчер"""
+import time
+import os
+from subprocess import Popen
 
-import subprocess
+CHOICE_TEXT = """
+1 - запуск сервера
+2 - остановка сервера
+3 - запуск 4 клиентов
+4 - остановка клиентов
+5 - остановить все и выйти
+Выберите действие: """
 
-PROCESSES = []
+CLIENTS = []
+SERVER = ''
+PATH_TO_FILE = os.path.dirname(__file__)
+PATH_TO_SCRIPT_SERVER = os.path.join(PATH_TO_FILE, "server.py")
+PATH_TO_SCRIPT_CLIENTS = os.path.join(PATH_TO_FILE, "client.py")
 
 while True:
-    ACTION = input('Выберите действие: q - выход, '
-                   's - запустить сервер и клиенты, '
-                   'x - закрыть все окна: ')
+    CHOICE = input(CHOICE_TEXT)
 
-    if ACTION == 'q':
+    if CHOICE == '1':
+        print("Запустили сервер")
+        SERVER = Popen(
+            f'osascript -e \'tell application "Terminal" to do'
+            f' script "python3 {PATH_TO_SCRIPT_SERVER}"\'', shell=True)
+    elif CHOICE == '2':
+        pass
+        # print("Убили сервер")
+        # SERVER.kill()
+    elif CHOICE == '3':
+        print("Запустили клиенты")
+        for i in range(2):
+            CLIENTS.append(
+                Popen(
+                    f'osascript -e \'tell application "Terminal" to do'
+                    f' script "python3 {PATH_TO_SCRIPT_CLIENTS}"\'',
+                    shell=True))
+            CLIENTS.append(
+                Popen(
+                    f'osascript -e \'tell application "Terminal" to do'
+                    f' script "python3 {PATH_TO_SCRIPT_CLIENTS} -m send"\'',
+                    shell=True))
+            # Задержка для того, что бы отправляющий процесс успел
+            # зарегистрироваться на сервере, и потом в словаре имен
+            # клиентов остался только слушающий клиент
+            time.sleep(0.5)
+    elif CHOICE == '4':
+        pass
+        # for i in range(len(CLIENTS)):
+        #     print(CLIENTS[i])
+        #     CLIENTS[i].kill()
+    elif CHOICE == '5':
         break
-    elif ACTION == 's':
-        PROCESSES.append(subprocess.Popen(['python3', 'server.py']))
-        PROCESSES.append(subprocess.Popen(['python3', 'client.py', '-n test1']))
-        PROCESSES.append(subprocess.Popen(['python3', 'client.py', '-n test2']))
-        PROCESSES.append(subprocess.Popen(['python3', 'client.py', '-n test3']))
-
-    elif ACTION == 'x':
-        while PROCESSES:
-            VICTIM = PROCESSES.pop()
-            VICTIM.kill()
+        # for i in range(len(CLIENTS)):
+        #     CLIENTS[i].kill()
+        # SERVER.kill()
+        # break
